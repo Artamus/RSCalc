1d54cd6204e159e1fa354a53d80337fca23764dd
Markus <markusloide@gmail.com>
2016-06-16 23:46:31 +0300

    Initial commit

---
+++ src/main/java/com/rscalc/MyController.java
@@ -0,0 +1,15 @@
+package com.rscalc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Markus on 16.06.2016.
+ */
+@Controller
+public class MyController {
+    @RequestMapping("test")
+    @ResponseBody
+    public String doGet() { return "Hello, World!"; }
+}
---
+++ src/main/java/com/rscalc/RsCalcApplication.java
@@ -0,0 +1,12 @@
+package com.rscalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsCalcApplication.class, args);
+	}
+}
